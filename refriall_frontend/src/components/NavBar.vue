13ba3e26796a321daa5f425e79132c0599af23d2
Johan Travieso Castro <jtravieso8863@gmail.com>
2023-10-17 20:44:05 -0400

    navbar component

---
+++ refriall_frontend/src/components/NavBar.vue
@@ -0,0 +1,49 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-lightblue">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">RefriAll</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Facturación
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Órdenes</a></li>
+                            <li><a class="dropdown-item" href="#">Facturas</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Almacén
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Artículos</a></li>
+                            <li><a class="dropdown-item" href="#">Equipos</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            RRHH
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Prestadores</a></li>
+                            <li><a class="dropdown-item" href="#">Clientes</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div class="ms-auto">
+                <a href="#" class="btn btn-sm btn-outline-danger">Salir</a>
+            </div>
+        </div>
+    </nav>
+</template>
--- refriall_frontend/src/main.js
+++ refriall_frontend/src/main.js
@@ -1,4 +1,5 @@
-import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/custom.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -12,3 +13,5 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+import 'bootstrap/dist/js/bootstrap.bundle'
