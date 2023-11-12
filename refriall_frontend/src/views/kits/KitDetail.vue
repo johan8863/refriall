0e42f89438a39bad96c5ecf15078f57e2378fd14
Johan Travieso Castro <jtravieso8863@gmail.com>
2023-11-12 11:54:56 -0500

    bootstrap modal

--- refriall_frontend/src/views/kits/KitDetail.vue
+++ refriall_frontend/src/views/kits/KitDetail.vue
@@ -13,7 +13,10 @@
                     <router-link :to="{name: 'kits_update', params: {id: kit.id}}">Editar</router-link>
                 </li>
                 <li class="list-group-item">
-                    <router-link :to="{name: 'kits_update', params: {id: kit.id}}">Eliminar</router-link>
+                    <a
+                      href="#"
+                      data-bs-toggle="modal"
+                      data-bs-target="#deleteKitModal">Eliminar</a>
                 </li>
             </ul>
         </div>
@@ -25,24 +28,73 @@
 
     </div> <!-- end row -->
 
+    <!-- Modal -->
+    <div class="modal fade" id="deleteKitModal" tabindex="-1" aria-labelledby="deleteKitModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="deleteKitModalLabel">Atención</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p>¿Está seguro de eliminar el equipo: {{kit.name}}?</p>
+            </div>
+            <div class="modal-footer">
+                <form method="post" @submit.prevent>
+                    <button
+                      class="btn btn-outline-danger btn-sm"
+                      @click="delKit(kit.id)">Eliminar</button>
+                    <button type="button" class="btn btn-outline-secondary btn-sm" data-bs-dismiss="modal">Cancelar</button>
+                </form>
+            </div>
+            </div>
+        </div>
+    </div>
+
 </template>
 
 <script setup>
-
+// vue
 import { onMounted, ref } from "vue";
-import { RouterLink, useRoute } from "vue-router";
-import { detatilKit } from "../../services/kit.service";
+import { RouterLink, useRoute, useRouter } from "vue-router";
 
-const route = useRoute();
+// third
+import "bootstrap";
+
+// local
+import { deleteKit, detatilKit } from "../../services/kit.service";
+import bootstrap from "bootstrap";
+
+// kit object
 const kit = ref({
     name: ''
 });
 
+// router utilities to redirect the view and catch route params
+const route = useRoute();
+const router = useRouter();
+
+// bootstrap modal
+const kitModal  = new bootstrap.Modal(document.getElementById('deleteKitModal'));
+
+
+// loading the kit object
 onMounted(async () => {
     const resp = await detatilKit(route.params.id);
     kit.value = resp.data;
 });
 
+// delete the kit object
+const delKit = async (id) => {
+    try {
+        await deleteKit(id);
+        // kitModal.dispose()
+        router.push({name: 'kits'});
+    } catch (error) {
+        // in case of backend errors, log to the console... for now
+        console.log(error.response.data);
+    }
+};
 
 </script>
 
