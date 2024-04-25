d91afe7c2b8d54c58dc9dd79ee72114780268a7c
Johan Travieso Castro <jtravieso8863@gmail.com>
2024-04-25 15:40:25 -0400

    orders ordering by provider_signature_date attr

--- refriall_backend/finance/models.py
+++ refriall_backend/finance/models.py
@@ -184,7 +184,7 @@ class Order(models.Model):
     aproved_by = models.CharField('Aprobado por', max_length=35, null=True, blank=True)
 
     class Meta:
-        # ordering = ['provider_signature_date']
+        ordering = ['provider_signature_date']
         verbose_name = 'Orden de Servicio'
         verbose_name_plural = 'Órdenes de Servicio'
     
