757a7c48d2dbc63504eff9e372e0e1618d107d50
Johan Travieso Castro <jtravieso8863@gmail.com>
2024-04-30 00:48:47 -0400

    order base details

--- refriall_frontend/src/views/orders/OrderDetail.vue
+++ refriall_frontend/src/views/orders/OrderDetail.vue
@@ -50,15 +50,15 @@
                     <div v-if="paginatedOrder.customer" class="col-md-9 border-bottom border-2 mb-1">
                         <span class="d-block">{{ paginatedOrder.customer.name }}</span>
                         <span class="d-block">{{ paginatedOrder.customer.address }}</span>
-                        <span class="d-block">{{ paginatedOrder.customer.bank_account_header }}</span>
-                        <span class="d-block">{{ paginatedOrder.customer.bank_account }}</span>
+                        <span class="d-block">{{ paginatedOrder.customer.province }}</span>
+                        <span class="d-block">{{ paginatedOrder.customer.township }}</span>
                     </div>
 
                     <div v-if="paginatedOrder.customer_dependency" class="col-md-9 border-bottom border-2 mb-1">
                         <span class="d-block">{{ paginatedOrder.customer_dependency.name }}</span>
                         <span class="d-block">{{ paginatedOrder.customer_dependency.address }}</span>
-                        <span class="d-block">{{ paginatedOrder.customer_dependency.get_bank_account_header }}</span>
-                        <span class="d-block">{{ paginatedOrder.customer_dependency.get_bank_account }}</span>
+                        <span class="d-block">{{ paginatedOrder.customer_dependency.province }}</span>
+                        <span class="d-block">{{ paginatedOrder.customer_dependency.township }}</span>
                     </div>
 
                     <div class="col-md-3 border-bottom border-2 mb-1">
