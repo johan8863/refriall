6251649c9abeea7fea17c5651c1538880953036c
Johan Travieso Castro <jtravieso8863@gmail.com>
2025-01-08 14:47:43 -0500

    only show the error or the information

--- refriall_frontend/src/views/orders/OrderList.vue
+++ refriall_frontend/src/views/orders/OrderList.vue
@@ -35,7 +35,7 @@ const getOrders = async () => {
         if (error.response) {
             orderBackendErrors.value = `${error.response.data} - ${error.response.status}`
         } else if (error.request) {
-            orderBackendErrors.value = 'El servidor back-end no responde, consulte al desarrollador'
+            orderBackendErrors.value = 'El servidor back-end no responde, consulte al desarrollador.'
         }
     }
 };
@@ -75,62 +75,67 @@ onMounted(async () => {
         <div class="col-md-10">
             <div class="row">
                 <div class="col-md-6">
-                    <div v-if="orderBackendErrors">
-                        <span class="form-text text-danger">{{ orderBackendErrors }}</span>
-                    </div>
-                    <div v-if="orders.length > 0">
+                    <div v-if="!orderBackendErrors">
 
-                        <div id="tableContainer">
-                            
-                            <table class="table">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">Orden</th>
-                                        <th scope="col">Cliente</th>
-                                        <th scope="col">Importe</th>
-                                        <th scope="col">Prefactura</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr v-for="order in orders" :key="order.id">
-                                        <td>
-                                            <router-link :to="{name: 'orders_detail', params: {id: order.id}}">{{ order.folio }}</router-link>
-                                        </td>
-                                        <td v-if="order.customer">
-                                            {{ order.customer.name }}
-                                        </td>
-                                        <td v-if="order.customer_dependency">
-                                            {{ order.customer_dependency.name }}
-                                        </td>
-                                        <td>
-                                            {{ order.get_total_amount.toFixed(2) }}
-                                        </td>
-                                        <td>
-                                            <router-link :to="{name: 'orders_detail_pre_order', params: {id: order.id}}">Detalle</router-link>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-
+                        <div v-if="orders.length > 0">
+    
+                            <div id="tableContainer">
+                                
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Orden</th>
+                                            <th scope="col">Cliente</th>
+                                            <th scope="col">Importe</th>
+                                            <th scope="col">Prefactura</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr v-for="order in orders" :key="order.id">
+                                            <td>
+                                                <router-link :to="{name: 'orders_detail', params: {id: order.id}}">{{ order.folio }}</router-link>
+                                            </td>
+                                            <td v-if="order.customer">
+                                                {{ order.customer.name }}
+                                            </td>
+                                            <td v-if="order.customer_dependency">
+                                                {{ order.customer_dependency.name }}
+                                            </td>
+                                            <td>
+                                                {{ order.get_total_amount.toFixed(2) }}
+                                            </td>
+                                            <td>
+                                                <router-link :to="{name: 'orders_detail_pre_order', params: {id: order.id}}">Detalle</router-link>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+    
+                            </div>
+    
+                            <!-- buttons -->
+                            <div class="mb-4">
+                                <button
+                                  v-if="showPrevButton"
+                                  class="btn btn-sm btn-primary"
+                                  @click="loadPrevItems()">Anterior</button>
+    
+                                <button
+                                  v-if="showNextButton"
+                                  class="btn btn-sm btn-primary"
+                                  @click="loadNextItems()">Siguiente</button>
+                            </div>
+    
                         </div>
-
-                        <!-- buttons -->
-                        <div class="mb-4">
-                            <button
-                              v-if="showPrevButton"
-                              class="btn btn-sm btn-primary"
-                              @click="loadPrevItems()">Anterior</button>
-
-                            <button
-                              v-if="showNextButton"
-                              class="btn btn-sm btn-primary"
-                              @click="loadNextItems()">Siguiente</button>
+    
+                        <div v-else>
+                            <p class="lead text-center">Inserte una orden.</p>
                         </div>
 
                     </div>
 
                     <div v-else>
-                        <p class="lead text-center">Inserte una orden.</p>
+                        <span class="form-text text-danger">{{ orderBackendErrors }}</span>
                     </div>
                     
                 </div>
