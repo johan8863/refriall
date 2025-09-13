9f87ad457d75b9fc9f873e4fdd6ebb9da278807b
Johan Travieso Castro <jtravieso8863@gmail.com>
2025-09-12 21:21:22 -0400

    fix(env): updated from process.env for import.meta.env

--- refriall_frontend/src/services/base.service.js
+++ refriall_frontend/src/services/base.service.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000';
+
 const apiBase = axios.create({
-    baseURL: process.env.VUE_APP_API_BASE_URL || 'http://127.0.0.1:8000'
+    baseURL: BASE_URL
 });
 
 export default apiBase;
