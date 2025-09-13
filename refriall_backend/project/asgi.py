439d8c19b47b45df1976e958a2a41685b51d4f15
Johan Travieso Castro <jtravieso8863@gmail.com>
2025-09-12 21:16:22 -0400

    feat(env): isolated 'DJANGO_SETTINGS_MODULE

--- refriall_backend/project/asgi.py
+++ refriall_backend/project/asgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+from decouple import config
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', config('DJANGO_SETTINGS_MODULE'))
 
 application = get_asgi_application()
--- refriall_backend/project/wsgi.py
+++ refriall_backend/project/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings.prod')
+from decouple import config
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', config('DJANGO_SETTINGS_MODULE'))
 
 application = get_wsgi_application()
