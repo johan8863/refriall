ad0d9fcbc0dfb367fade7e5912257f1b0e609192
Johan Travieso Castro <jtravieso8863@gmail.com>
2025-09-19 06:04:32 -0400

    feat(api): provider serializer custom validations

--- refriall_backend/hr/serializers.py
+++ refriall_backend/hr/serializers.py
@@ -43,6 +43,36 @@ class ProviderSerializerWrite(serializers.ModelSerializer):
             "password",
             "personal_id",
         ]
+    
+    def validate_all_digits(self, attr):
+        """validates all characters in attr are digits"""
+
+        str_attr = str(attr)
+        return str_attr.isdigit()
+    
+    def validate_license_number(self, value):
+        """license_number attr must be exactly 16 characters long"""
+
+        license_number_len = len(value)
+
+        if not self.validate_all_digits(value):
+            raise serializers.ValidationError("La licencia debe contener sólo números.")
+
+        if license_number_len is not 16:
+            raise serializers.ValidationError("La licencia debe contener 16 caracteres.")
+        return value
+    
+    def validate_personal_id(self, value):
+        """personal_id attr must be exactly 11 characters long"""
+
+        personal_id_len = len(value)
+
+        if not self.validate_all_digits(value):
+            raise serializers.ValidationError("El CI debe contener sólo números.")
+            
+        if personal_id_len is not 11:
+            raise serializers.ValidationError("El CI debe contener 16 caracteres.")
+        return value
 
 
 class CustomerDependencySerializer(serializers.ModelSerializer):
