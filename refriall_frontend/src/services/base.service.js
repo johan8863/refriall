e2dd9a6c207fe2ea8cc23d5ba9e180372e66a7ce
Johan Travieso Castro <jtravieso8863@gmail.com>
2026-02-04 15:12:27 -0500

    feat: update access token

--- refriall_frontend/src/services/base.service.js
+++ refriall_frontend/src/services/base.service.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const REFRESH_URL = import.meta.env.VITE_REFRESH_TOKEN_URL
 
 const apiBase = axios.create({
     baseURL: BASE_URL,
@@ -18,6 +19,42 @@ apiBase.interceptors.request.use(
         return config
     },
     (error) => Promise.reject(error),
+)
+
+apiBase.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config
+        if (error.response.status === 401 && !originalRequest._retry) {
+            try {
+                originalRequest._retry = true
+                const refreshToken = localStorage.getItem('refriall_auth_refresh_token')
+                if (!refreshToken) {
+                    throw new Error("No refresh token available")
+                }
+                const { data } = await apiBase.post(REFRESH_URL, {refresh: refreshToken})
+                const { access } = data
+
+                localStorage.setItem('refriall_auth_access_token', access)
+
+                originalRequest.headers.Authorization = `Bearer ${access}`
+
+                return apiBase(originalRequest)
+            } catch (refreshError) {
+                console.error("Token refreshfailed", refreshError)
+
+                localStorage.removeItem('refriall_auth_access_token')
+                localStorage.removeItem('refriall_auth_refresh_token')
+
+                if (window.location.href !== '/login') {
+                    window.location.href = '/login'
+                }
+
+                return Promise.reject(refreshError)
+            }
+        }
+        return Promise.reject(error)
+    }
 )
 
 export default apiBase;
