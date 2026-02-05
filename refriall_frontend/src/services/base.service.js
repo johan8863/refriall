0252fcb5b35ac9e1495bcc6a4cbadf23974059af
Johan Travieso Castro <jtravieso8863@gmail.com>
2026-02-05 17:08:44 -0500

    fix: remove redundant logic

--- refriall_frontend/src/services/base.service.js
+++ refriall_frontend/src/services/base.service.js
@@ -1,77 +1,78 @@
-import axios from "axios";
+import axios from 'axios'
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const BASE_URL = import.meta.env.VITE_API_BASE_URL
 const REFRESH_URL = import.meta.env.VITE_REFRESH_TOKEN_URL
 
+const clearStateAndStorage = async () => {
+  const { useAuthStore } = await import('../stores/authStore')
+  const authStore = useAuthStore()
+  authStore.isAuthenticated = false
+  localStorage.removeItem('refriall_auth_access_token')
+  localStorage.removeItem('refriall_auth_refresh_token')
+
+  if (window.location.pathname !== '/login') {
+    const currentPath = window.location.pathname
+    window.location.href = `/login?redirect=${currentPath}`
+  }
+}
+
 const apiBase = axios.create({
-    baseURL: BASE_URL,
-    headers: {
-        'Content-Type': 'application/json'
-    }
-});
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
 
 apiBase.interceptors.request.use(
-    (config) => {
-        const access = localStorage.getItem('refriall_auth_access_token')
-        if (access) {
-            config.headers.Authorization = `Bearer ${access}`
-        }
-        return config
-    },
-    (error) => Promise.reject(error),
+  (config) => {
+    const access = localStorage.getItem('refriall_auth_access_token')
+    if (access) {
+      config.headers.Authorization = `Bearer ${access}`
+    }
+    return config
+  },
+  (error) => Promise.reject(error)
 )
 
 apiBase.interceptors.response.use(
-    (response) => response,
-    async (error) => {
-        const originalRequest = error.config
-        if (error.response.status === 401) {
-            if (!originalRequest._retry) {
-                try {
-                    originalRequest._retry = true
-                    
-                    const refreshToken = localStorage.getItem('refriall_auth_refresh_token')
-                    if (!refreshToken) {
-                        throw new Error("No refresh token available")
-                    }
-    
-                    if (originalRequest.url.includes(REFRESH_URL)) {
-                        localStorage.removeItem('refriall_auth_access_token')
-                        localStorage.removeItem('refriall_auth_refresh_token')
-    
-                        if (window.location.href !== '/login') {
-                            window.location.href = '/login'
-                        }
-    
-                        return Promise.reject(refreshError)
-                    }
-                    
-                    const { data } = await apiBase.post(REFRESH_URL, {refresh: refreshToken})
-                    const { access } = data
-    
-                    localStorage.setItem('refriall_auth_access_token', access)
-    
-                    originalRequest.headers.Authorization = `Bearer ${access}`
-    
-                    return apiBase(originalRequest)
-                } catch (refreshError) {
-                    console.error("Token refreshfailed", refreshError)
-    
-                    localStorage.removeItem('refriall_auth_access_token')
-                    localStorage.removeItem('refriall_auth_refresh_token')
-    
-                    if (window.location.href !== '/login') {
-                        window.location.href = '/login'
-                    }
-    
-                    return Promise.reject(refreshError)
-                }
-            }
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config
+
+    if (error.response?.status === 401) {
+      if (!originalRequest._retry) {
+        try {
+          originalRequest._retry = true
+
+          const refreshToken = localStorage.getItem('refriall_auth_refresh_token')
+          if (!refreshToken) {
+            throw new Error('No refreshtoken available')
+          }
+
+          if (originalRequest.url.includes(REFRESH_URL)) {
+            // clear state and storage
+            await clearStateAndStorage()
+            return Promise.reject(error)
+          }
+
+          const { data } = await apiBase.post(REFRESH_URL, { refresh: refreshToken })
+          const { access } = data
+
+          localStorage.setItem('refriall_auth_access_token', access)
+
+          originalRequest.headers.Authorization = `Bearer ${access}`
+
+          return apiBase(originalRequest)
+        } catch (refreshError) {
+          // clear state and storage
+          await clearStateAndStorage()
+          return Promise.reject(refreshError)
         }
-        console.error("General response interceptor error", error);
-        
-        return Promise.reject(error)
+      }
     }
+
+    return Promise.reject(error)
+  }
 )
 
-export default apiBase;
+export default apiBase
