d6879608681f25af5e805f0b482de519db5f53ea
Johan <jtravieso8863@gmail.com>
2026-03-29 11:17:31 -0500

    feat: loading state in currency detail

--- refriall_frontend/src/views/currencies/CurrencyDetail.vue
+++ refriall_frontend/src/views/currencies/CurrencyDetail.vue
@@ -1,68 +1,87 @@
 <script setup>
-
 // vue
-import { ref, onMounted } from "vue";
-import { useRoute } from "vue-router";
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
 
 // app
-import { currencyService } from "../../services/currencyService";
-import listGroup from "../../assets/js/bootstrap_classes/listGroup";
+import { currencyService } from '../../services/currencyService'
+import listGroup from '../../assets/js/bootstrap_classes/listGroup'
+import { errorHandler } from '../../utils/errors/errorHandler'
 
 const currency = ref({
-    name: '',
-    description: '',
-});
+  name: '',
+  description: ''
+})
 
-const currencyBackenderror = ref(null);
+// loading state
+const isLoading = ref(false)
 
-const route = useRoute();
+const currencyBackenderror = ref(null)
+
+const route = useRoute()
 
 onMounted(async () => {
-    try {
-        const response = await currencyService.detailCurrency(route.params.id);
-        currency.value = response.data
-    } catch (error) {
-        console.error('General error', error)
-        if (error.response) {
-            currencyBackenderror.value = `${error.response.data.detail} - ${error.response.status}`
-        } else {
-            currencyBackenderror.value = 'Error inesperado, consulte al desarrollador'
-        }
+  try {
+    // start loading state
+    isLoading.value = true
+    // get currency data
+    const response = await currencyService.detailCurrency(route.params.id)
+    currency.value = response.data
+  } catch (error) {
+    console.error('General error', error)
+    if (error.response) {
+      errorHandler(error, currencyBackenderror, 'Moneda')
     }
-});
-
+  } finally {
+    isLoading.value = false
+  }
+})
 </script>
 
 <template>
-    <div class="row">
-        <!-- side menu -->
-        <div class="col-md-2">
-            <ul :class="listGroup.listGroup">
-                <li :class="listGroup.listGroupItem">
-                    <strong>Monedas</strong>
-                </li>
-                <li :class="listGroup.listGroupItem">
-                    <router-link :to="{name: 'currencies'}">Monedas</router-link>
-                </li>
-                <li :class="listGroup.listGroupItem">
-                    <router-link :to="{name: 'currency_update', params: {id: currency.id}}">Actualizar</router-link>
-                </li>
-                <li :class="listGroup.listGroupItem">
-                    <router-link :to="{name: 'currency_delete', params: {id: currency.id}}">Eliminar</router-link>
-                </li>
-            </ul>
-        </div>
-        <!-- main content -->
-        <div class="col-md-4">
-            <!-- backend general errors -->
-            <span v-if="currencyBackenderror">
-                <p
-                    class="form-text text-danger">
-                    {{ currencyBackenderror }}</p>
-            </span>
-            <h3>{{ currency.name }}</h3>
-            <p>{{ currency.description }}</p>
-        </div>
-
+  <div class="row">
+    <!-- side menu -->
+    <div class="col-md-2">
+      <ul :class="listGroup.listGroup">
+        <li :class="listGroup.listGroupItem">
+          <strong>Monedas</strong>
+        </li>
+        <li :class="listGroup.listGroupItem">
+          <router-link :to="{ name: 'currencies' }">Monedas</router-link>
+        </li>
+        <li :class="listGroup.listGroupItem">
+          <router-link :to="{ name: 'currency_update', params: { id: currency.id } }"
+            >Actualizar</router-link
+          >
+        </li>
+        <li :class="listGroup.listGroupItem">
+          <router-link :to="{ name: 'currency_delete', params: { id: currency.id } }"
+            >Eliminar</router-link
+          >
+        </li>
+      </ul>
     </div>
+
+    <!-- main content -->
+
+    <!-- loading order data -->
+    <div v-if="isLoading" class="col-md-4">
+      <div class="d-flex justify-content-center align-items-center" style="min-height: 200px">
+        <span role="status" class="text-primary">Cargando datos... </span>
+        <span class="spinner-border spinner-border-sm text-primary" aria-hidden="true"></span>
+      </div>
+    </div>
+
+    <!-- displaying currency data -->
+    <div v-else class="col-md-4">
+      <!-- backend general errors -->
+      <span v-if="currencyBackenderror">
+        <p class="form-text text-danger">
+          {{ currencyBackenderror }}
+        </p>
+      </span>
+      <h3>{{ currency.name }}</h3>
+      <p>{{ currency.description }}</p>
+    </div>
+  </div>
 </template>
