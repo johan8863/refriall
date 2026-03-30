3efe2404b1db10885228ac6d4870bf4c691a9500
Johan <jtravieso8863@gmail.com>
2026-03-30 16:24:09 -0500

    feat: loading state in kit detail

--- refriall_frontend/src/views/kits/KitDetail.vue
+++ refriall_frontend/src/views/kits/KitDetail.vue
@@ -6,6 +6,7 @@ import { useRoute } from "vue-router";
 // app
 import { kitService } from "../../services/kitService";
 import listGroup from "../../assets/js/bootstrap_classes/listGroup";
+import { errorHandler } from "../../utils/errors/errorHandler";
 
 
 // kit object
@@ -21,22 +22,22 @@ const kitErrors = ref({
 // router utilities to redirect the view and catch route params
 const route = useRoute();
 
+// loading state
+const isLoading = ref(false)
 
 // loading the kit object
 onMounted(async () => {
     try {
+        // start loading state
+        isLoading.value = true
+        // getting kit data from backend
         const resp = await kitService.detailKit(route.params.id);
         kit.value = resp.data;
     } catch (error) {
-        console.error('General error', error)
-        if (error.response) {
-            kitErrors.value = `${error.response.data.detail} - ${error.response.status}`
-            console.log('response', kitErrors.value);
-            
-        } else {
-            kitErrors.value = 'Error inesperado, consulte al desarrollador'
-            console.log('else', kitErrors.value);
-        }
+        errorHandler(error, kitErrors, 'Equipo', 'm')
+    } finally {
+        // stop loading state
+        isLoading.value = false
     }
 });
 
@@ -66,7 +67,17 @@ onMounted(async () => {
         </div>
 
         <!-- main content -->
-        <div class="col-md-4">
+
+        <!-- loading kit data -->
+        <div v-if="isLoading" class="col-md-6">
+            <div class="d-flex justify-content-center align-items-center" style="min-height: 200px">
+                <span role="status" class="text-primary">Cargando datos... </span>
+                <span class="spinner-border spinner-border-sm text-primary" aria-hidden="true"></span>
+            </div>
+        </div>
+
+        <!-- displaying kit data -->
+        <div v-else class="col-md-4">
             <!-- backend general errors -->
             <span v-if="kitErrors.name">
                 <p
