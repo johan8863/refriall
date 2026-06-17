f2a34f44cc8fd433c0b7e303a0979c4800b7ca91
Johan Travieso Castro <jtravieso8863@gmail.com>
2026-06-17 08:33:58 -0400

    fix: import module accurately

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './assets/custom.css'
 
 import { createApp } from 'vue'
