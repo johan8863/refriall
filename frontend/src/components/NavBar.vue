0ab2bfb0b7daa1a47f6678d005a1c5b2281c1a45
Johan Travieso Castro <jtravieso8863@gmail.com>
2026-06-28 14:36:41 -0400

    navbar brand points to orders list view

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -22,7 +22,7 @@ const logout = () => {
 <template>
     <nav class="navbar navbar-expand-lg bg-lightblue">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">RefriAll</a>
+            <RouterLink class="navbar-brand" :to="{name: 'orders'}">RefriAll</RouterLink>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
