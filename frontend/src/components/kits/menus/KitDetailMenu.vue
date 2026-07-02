bd09b8a15ae3d44aa66ca6af39e0f7a36520de2a
Johan Travieso Castro <jtravieso8863@gmail.com>
2026-07-01 20:09:20 -0400

    feat: kit detail menu component

---
+++ frontend/src/components/kits/menus/KitDetailMenu.vue
@@ -0,0 +1,29 @@
+<script setup>
+defineProps({
+  kit: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+<template>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">
+      <strong>Equipos</strong>
+    </li>
+    <li class="list-group-item">
+      <router-link :to="{ name: 'kits' }">Equipos</router-link>
+    </li>
+    <li class="list-group-item">
+      <router-link :to="{ name: 'kits_create' }">Nuevo</router-link>
+    </li>
+    <li class="list-group-item">
+      <router-link :to="{ name: 'kits_update', params: { id: kit.id } }">Editar</router-link>
+    </li>
+    <li class="list-group-item">
+      <router-link :to="{ name: 'kits_confirm_delete', params: { id: kit.id } }"
+        >Eliminar</router-link
+      >
+    </li>
+  </ul>
+</template>
--- frontend/src/views/kits/KitDetail.vue
+++ frontend/src/views/kits/KitDetail.vue
@@ -5,8 +5,8 @@ import { useRoute } from 'vue-router'
 
 // app
 import { kitService } from '../../services/kitService'
-import listGroup from '../../assets/js/bootstrap_classes/listGroup'
 import { errorHandler } from '../../utils/errors/errorHandler'
+import KitDetailMenu from '../../components/kits/menus/KitDetailMenu.vue'
 
 // kit object
 const kit = ref({
@@ -45,25 +45,7 @@ onMounted(async () => {
   <div class="row">
     <!-- side menu -->
     <div class="col-md-2">
-      <ul :class="listGroup.listGroup">
-        <li :class="listGroup.listGroupItem">
-          <strong>Equipos</strong>
-        </li>
-        <li :class="listGroup.listGroupItem">
-          <router-link :to="{ name: 'kits' }">Equipos</router-link>
-        </li>
-        <li :class="listGroup.listGroupItem">
-          <router-link :to="{ name: 'kits_create' }">Nuevo</router-link>
-        </li>
-        <li :class="listGroup.listGroupItem">
-          <router-link :to="{ name: 'kits_update', params: { id: kit.id } }">Editar</router-link>
-        </li>
-        <li :class="listGroup.listGroupItem">
-          <router-link :to="{ name: 'kits_confirm_delete', params: { id: kit.id } }"
-            >Eliminar</router-link
-          >
-        </li>
-      </ul>
+      <kit-detail-menu :kit="kit" />
     </div>
 
     <!-- main content -->
