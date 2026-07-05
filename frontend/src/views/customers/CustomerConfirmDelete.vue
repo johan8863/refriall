21758a79c60cc3588a6c8ba082c7abe364249fb3
Johan Travieso Castro <jtravieso8863@gmail.com>
2026-07-05 13:38:08 -0400

    feat: loading state

--- frontend/src/views/customers/CustomerConfirmDelete.vue
+++ frontend/src/views/customers/CustomerConfirmDelete.vue
@@ -21,6 +21,8 @@ const customer = ref({
   bank_account: ''
 })
 
+const isLoading = ref(false)
+
 // errors holder object
 const errorMessage = ref(null)
 
@@ -31,6 +33,9 @@ const route = useRoute()
 // lifecycle
 onMounted(async () => {
   try {
+    // start loading state
+    isLoading.value = true
+
     const resp = await customerService.detailCustomer(route.params.id)
     customer.value = resp.data
   } catch (error) {
@@ -43,6 +48,9 @@ onMounted(async () => {
     } else if (error.request) {
       errorMessage.value = 'Error del Servidor, consulte al desarrollador.'
     }
+  } finally {
+    // stop loading state
+    isLoading.value = false
   }
 })
 
@@ -75,14 +83,24 @@ const delCustomer = async (id) => {
       <CustomerConfirmDeleteMenu :customer="customer" />
     </div>
 
-    <!-- main content -->
-    <div class="col-md-4">
-      <!-- backend general errors -->
+    <!-- loading state -->
+    <div v-if="isLoading" class="col-md-4">
+      <div class="d-flex justify-content-center align-items-center" style="min-height: 200px">
+        <span role="status" class="text-primary">Cargando datos... </span>
+        <span class="spinner-border spinner-border-sm text-primary" aria-hidden="true"></span>
+      </div>
+    </div>
+
+    <!-- error message -->
+    <div v-else-if="errorMessage" class="col-md-4">
       <span v-if="errorMessage">
         <p class="form-text text-danger">
           {{ errorMessage }}
         </p>
       </span>
+    </div>
+
+    <div v-else class="col-md-4">
       <p>Está seguro que desea eliminar el cliente: {{ customer.name }}?</p>
       <div>
         <button class="btn btn-sm btn-danger" @click="delCustomer(customer.id)">Eliminar</button>
