c8f0370756b2f6fc0f5ed0308fb6299dd2190401
Johan Travieso Castro <jtravieso8863@gmail.com>
2026-07-07 09:39:41 -0400

    feat: loading state

--- frontend/src/views/currencies/CurrencyConfirmDelete.vue
+++ frontend/src/views/currencies/CurrencyConfirmDelete.vue
@@ -19,6 +19,9 @@ const currency = ref({
   description: ''
 })
 
+// loading state
+const isLoading = ref(false)
+
 // errors
 const errorMessage = ref(null)
 
@@ -34,9 +37,15 @@ const delCurrency = async () => {
 
 onMounted(async () => {
   try {
+    // start loading state
+    isLoading.value = true
+    // getting data from backend
     currency.value = (await currencyService.detailCurrency(route.params.id)).data
   } catch (error) {
     errorHandler(error, errorMessage, 'Moneda')
+  } finally {
+    // stop loading state
+    isLoading.value = false
   }
 })
 </script>
@@ -48,7 +57,20 @@ onMounted(async () => {
       <CurrencyConfirmDeleteMenu :currency="currency" />
     </div>
     <!-- main content -->
-    <div class="col-md-4">
+    <!-- loading state -->
+    <div v-if="isLoading" class="col-md-4">
+      <div class="d-flex justify-content-center align-items-center" style="min-height: 200px">
+        <span role="status" class="text-primary">Cargando datos... </span>
+        <span class="spinner-border spinner-border-sm text-primary" aria-hidden="true"></span>
+      </div>
+    </div>
+    <!-- error message -->
+    <div v-else-if="errorMessage" class="col-md-4">
+      <span class="form-text text-danger">
+        {{ errorMessage }}
+      </span>
+    </div>
+    <div v-else class="col-md-4">
       <!-- backend general errors -->
       <span v-if="errorMessage">
         <p class="form-text text-danger">
