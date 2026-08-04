20b3618123c5d8fb3677da4926515c256dc52d89
Johan Travieso Castro <jtravieso8863@gmail.com>
2026-08-04 10:28:43 -0400

    feat: CustomerDependencySelector no uses v-model prop

--- frontend/src/views/orders/OrderForm.vue
+++ frontend/src/views/orders/OrderForm.vue
@@ -384,14 +384,13 @@ onMounted(async () => {
 
         <!-- customer control -->
         <div class="col-md-3 mb-2">
-          <customer-dependency-selector
+          <CustomerDependencySelector
             type="customer"
             :customers="customers"
-            :model-value="order.customer"
+            v-model="order.customer"
             :disabled="!!order.customer_dependency"
             :vuelidate-errors="v$.customer.$errors"
             :get-field-errors="getFieldErrors"
-            @update:model-value="order.customer = $event"
             @blur="v$.customer.$touch"
             @clear="order.customer = ''"
           />
@@ -399,15 +398,14 @@ onMounted(async () => {
 
         <!-- customer_dependency control -->
         <div class="col-md-3 mb-2">
-          <customer-dependency-selector
+          <CustomerDependencySelector
             type="dependency"
             :dependencies="dependencies"
-            :model-value="order.customer_dependency"
+            v-model="order.customer_dependency"
             :disabled="!!order.customer"
             :vuelidate-errors="v$.customer_dependency.$errors"
             :get-field-errors="getFieldErrors"
-            @update:model-value="order.customer_dependency = $event"
-            @blur="v$.customer_dependency.$touch"
+            @blur="v$.customer.$touch"
             @clear="order.customer_dependency = ''"
           />
         </div>
