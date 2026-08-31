82c84513e84cd10f293accb9205d20b507bd3b8d
Johan Travieso Castro <jtravieso8863@gmail.com>
2026-08-31 18:51:12 -0400

    feat: kit service migrated to typescript

---
+++ frontend/src/services/kitService.ts
@@ -0,0 +1,65 @@
+import { kitAPIEnvs } from '../settings/env'
+import apiBase from './baseService'
+import type { Kit } from '../views/kits/types'
+import type { PaginatedResponse } from '../types/shared'
+
+const urlKit = kitAPIEnvs.kitUrl
+
+export const kitService = {
+  /*
+   * Paginated list kit service with optional search term
+   * @params currentPage - page number
+   * @params searchTerm - search term
+   * @returns Promise with a paginated response
+   */
+  listKit: (page: number = 1, search: string = ''): Promise<{ data: PaginatedResponse<Kit> }> => {
+    const params: Record<string, string | number> = {
+      ...(page && { page }),
+      ...(search && { search })
+    }
+    return apiBase.get(`${urlKit}/list-pagination/`, { params })
+  },
+  /*
+   * Searches kits based on the searchTerm param
+   * @param searchTerm - search term
+   * @param page - page number(optional, default = 1)
+   */
+  searchKits: (searchTerm: string, page: number = 1): Promise<{ data: PaginatedResponse<Kit> }> => {
+    const params: Record<string, string | number> = {
+      search: searchTerm,
+      ...(page > 1 && { page })
+    }
+    return apiBase.get(`${urlKit}/list-pagination/`, { params })
+  },
+  /*
+   * Gets all kits(no pagination)
+   * @returns Promise with the whole kits list
+   */
+  getAllKits: (): Promise<{ data: Kit[] }> => apiBase.get(`${urlKit}/`),
+  /*
+   * Gets a Kit by id
+   * @params id - Kit id
+   * @returns Promise with Kit data
+   */
+  detailKit: (id: number): Promise<{ data: Kit }> => apiBase.get(`${urlKit}/${id}/`),
+  /*
+   * Creates a Kit from kit param
+   * @param kit - Kit data
+   * @returns Promise with Kit created data
+   */
+  postKit: (kit: Omit<Kit, 'id'>): Promise<{ data: Kit }> => apiBase.post(`${urlKit}/`, kit),
+  /*
+   * Updates a Kit from kit param
+   * @param kit - Kit data
+   * @returns Promise with Kit updated data
+   */
+  putKit: (kit: Kit): Promise<{ data: Kit }> => apiBase.put(`${urlKit}/${kit.id}/`, kit),
+  /*
+   * Deletes a Kit by its id
+   * @params id - Kit id
+   * @returns Promise void
+   */
+  deleteKit: (id: number): Promise<void> => apiBase.delete(`${urlKit}/${id}/`)
+}
+
+export type { Kit }
