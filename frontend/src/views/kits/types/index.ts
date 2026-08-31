79752c5e4f63ef5720ebf3ffae11fa2370b0e515
Johan Travieso Castro <jtravieso8863@gmail.com>
2026-08-31 18:51:34 -0400

    feat: Kit types

---
+++ frontend/src/views/kits/types/index.ts
@@ -0,0 +1,22 @@
+import type { PaginatedResponse, FormErrors } from '../../../types/shared'
+
+/*
+ * Kit model
+ */
+export interface Kit {
+  id?: number
+  name: string
+}
+
+/*
+ * Errors for Kit form
+ */
+export type KitFormErrors = FormErrors<Kit>
+
+/*
+ * Kit interface to pass to detail menu component
+ */
+export interface KitDetailMenuProps {
+  kit: Kit
+  isLoading: boolean
+}
