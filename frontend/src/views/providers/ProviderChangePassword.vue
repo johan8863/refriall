cd10e66c554098be3c6232d5b1490be56bcec73e
Johan Travieso Castro <jtravieso8863@gmail.com>
2026-09-06 11:56:26 -0400

    feat: provider change password form to typescript

--- frontend/src/views/providers/ProviderChangePassword.vue
+++ frontend/src/views/providers/ProviderChangePassword.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 // vue
 import { onMounted, ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
@@ -9,47 +9,46 @@ import { required, helpers } from '@vuelidate/validators'
 
 // app
 import ProviderChangeSelfPasswordMenu from '@/components/providers/menus/ProviderChangeSelfPasswordMenu.vue'
-import { providerService } from '@/services/providerService.js'
+import { providerService } from '@/services/providerService'
+import type { ProviderChangePassword, PasswordChangeErrors } from './types'
 
-// main object
-const providerChangePassword = ref({
-  new_password: '',
-  confirm_new_password: ''
+// Main object
+const providerChangePassword = ref<ProviderChangePassword>({
+  confirm_new_password: '',
+  new_password: ''
 })
 
-// object backend errors
-const backendErrors = ref({
-  new_password: [],
+// Backend errors
+const backendErrors = ref<PasswordChangeErrors>({
   confirm_new_password: [],
+  new_password: [],
   network: ''
 })
 
-// routing utilities
+// Routing
 const router = useRouter()
 const route = useRoute()
 
-// rules to manage front validations
+// Validation rules
 const rules = {
-  new_password: {
+  confirm_new_password: {
     required: helpers.withMessage('La nueva clave es requerida.', required)
   },
-  confirm_new_password: {
+  new_password: {
     required: helpers.withMessage('La confirmación es requerida.', required)
   }
 }
 
-// vuelidate object
+// Vuelidate
 const v$ = useVuelidate(rules, providerChangePassword)
 
-// change password function
-const handleChangePassword = async () => {
+// Change password function
+const handleChangePassword = async (): Promise<void> => {
   try {
     if (await v$.value.$validate()) {
-      await providerService.changePassword(route.params.id, providerChangePassword.value)
+      await providerService.changePassword(Number(route.params.id), providerChangePassword.value)
       router.push({ name: 'providers_detail', params: { id: route.params.id } })
     } else {
-      // always log vuelidate erros to de console
-      // just in case of unexpected behavior
       console.error(
         v$.value.$errors.map((err) => ({
           property: err.$property,
@@ -57,7 +56,7 @@ const handleChangePassword = async () => {
         }))
       )
     }
-  } catch (error) {
+  } catch (error: any) {
     console.error({ error })
     if (error.response) {
       backendErrors.value = error.response.data
@@ -73,69 +72,39 @@ onMounted(() => console.log(route.params.id))
 
 <template>
   <div class="row">
-    <!-- side menu -->
     <div class="col-sm-2 col-md-2">
-      <provider-change-self-password-menu />
+      <ProviderChangeSelfPasswordMenu />
     </div>
 
-    <!-- main content -->
     <div class="col-md-2 col-sm-3">
-      <!-- backend errors from non_field_errors dictionary -->
-      <span v-if="backendErrors.non_field_errors">
+      <!-- Non-field errors -->
+      <div v-if="backendErrors.non_field_errors">
         <p
           class="form-text text-danger"
-          v-for="(error, index) in kitErrors.non_field_errors"
+          v-for="(error, index) in backendErrors.non_field_errors"
           :key="index"
         >
           {{ error }}
         </p>
-      </span>
-      <!-- network error -->
-      <span v-if="backendErrors.network">
+      </div>
+
+      <!-- Network error -->
+      <div v-if="backendErrors.network">
         <p class="form-text text-danger">{{ backendErrors.network }}</p>
-      </span>
+      </div>
+
       <form @submit.prevent="handleChangePassword">
-        <!-- new_password control -->
+        <!-- New password -->
         <div class="mb-3">
-          <label for="new_password" class="form-label">Nueva clave</label>
+          <label for="confirm_new_password" class="form-label">Nueva clave</label>
           <input
             autofocus
-            type="password"
-            id="new_password"
-            class="form-control form-control-sm"
-            v-model="providerChangePassword.new_password"
-            @blur="v$.new_password.$touch"
-          />
-          <!-- frontend validations -->
-          <p
-            class="form-text text-danger"
-            v-for="error in v$.new_password.$errors"
-            :key="error.$uid"
-          >
-            {{ error.$message }}
-          </p>
-          <!-- backend validations -->
-          <span v-if="backendErrors.new_password">
-            <p
-              class="form-text text-danger"
-              v-for="(error, index) in backendErrors.new_password"
-              :key="index"
-            >
-              {{ error }}
-            </p>
-          </span>
-        </div>
-        <!-- confirm_new_password control -->
-        <div class="mb-3">
-          <label for="confirm_new_password" class="form-label">Confirmar nueva clave</label>
-          <input
             type="password"
             id="confirm_new_password"
             class="form-control form-control-sm"
             v-model="providerChangePassword.confirm_new_password"
             @blur="v$.confirm_new_password.$touch"
           />
-          <!-- frontend validations -->
           <p
             class="form-text text-danger"
             v-for="error in v$.confirm_new_password.$errors"
@@ -143,8 +112,7 @@ onMounted(() => console.log(route.params.id))
           >
             {{ error.$message }}
           </p>
-          <!-- backend validations -->
-          <span v-if="backendErrors.confirm_new_password">
+          <div v-if="backendErrors.confirm_new_password">
             <p
               class="form-text text-danger"
               v-for="(error, index) in backendErrors.confirm_new_password"
@@ -152,16 +120,46 @@ onMounted(() => console.log(route.params.id))
             >
               {{ error }}
             </p>
-          </span>
+          </div>
         </div>
-        <!-- buttons -->
+
+        <!-- Confirm new password -->
+        <div class="mb-3">
+          <label for="new_password" class="form-label">Confirmar nueva clave</label>
+          <input
+            type="password"
+            id="new_password"
+            class="form-control form-control-sm"
+            v-model="providerChangePassword.new_password"
+            @blur="v$.new_password.$touch"
+          />
+          <p
+            class="form-text text-danger"
+            v-for="error in v$.new_password.$errors"
+            :key="error.$uid"
+          >
+            {{ error.$message }}
+          </p>
+          <div v-if="backendErrors.new_password">
+            <p
+              class="form-text text-danger"
+              v-for="(error, index) in backendErrors.new_password"
+              :key="index"
+            >
+              {{ error }}
+            </p>
+          </div>
+        </div>
+
+        <!-- Buttons -->
         <div>
-          <button class="btn btn-sm btn-primary">Cambiar</button>
+          <button type="submit" class="btn btn-sm btn-primary">Cambiar</button>
           <router-link
             class="btn btn-sm btn-secondary"
             :to="{ name: 'providers_detail', params: { id: route.params.id } }"
-            >Cancelar</router-link
           >
+            Cancelar
+          </router-link>
         </div>
       </form>
     </div>
