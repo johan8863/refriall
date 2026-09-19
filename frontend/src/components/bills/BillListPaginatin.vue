feeb58204d53f36e1ff52c59ba73bbaf20ec3c7f
Johan Travieso Castro <jtravieso8863@gmail.com>
2026-09-19 11:57:17 -0400

    feat: bill list pagination component typescript

--- frontend/src/components/bills/BillListPaginatin.vue
+++ frontend/src/components/bills/BillListPaginatin.vue
@@ -1,25 +1,17 @@
-<script setup>
-defineProps({
-  showPrevButton: {
-    type: Boolean,
-    required: true
-  },
-  showNextButton: {
-    type: Boolean,
-    required: true
-  },
-  currentPage: {
-    type: Number,
-    required: true
-  },
-  isLoading: {
-    type: Boolean,
-    required: true
-  }
-})
+<script setup lang="ts">
+defineProps<{
+  showPrevButton: boolean
+  showNextButton: boolean
+  currentPage: number
+  isLoading: boolean
+}>()
 
-defineEmits(['onLoadPrevItems', 'onLoadNextItems'])
+defineEmits<{
+  (e: 'onLoadPrevItems'): void
+  (e: 'onLoadNextItems'): void
+}>()
 </script>
+
 <template>
   <div class="d-flex justify-content-between align-items-center mt-2">
     <button
